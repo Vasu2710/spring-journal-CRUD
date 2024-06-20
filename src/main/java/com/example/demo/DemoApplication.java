2c361ca586473b9dbb31adddf5ffd7c00efaf91d
Vasu Bansal <vasubansal2002@gmail.com>
2024-06-20 17:27:28 +0530

    Added mongo-atlas for effective transactions.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableMongoRepositories
 public class DemoApplication {
 
 	public static void main(String[] args) {
